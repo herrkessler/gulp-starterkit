3b4234ed371f25065dca4f5f5524178aa69e54b0
Sebastian Kessler <sebastian@herrkessler.de>
2015-05-21 16:32:56 +0200

    "Major" Update
    added browsersync, autoprefixer, bower, better terminal colors…

--- gulpfile.js
+++ gulpfile.js
@@ -1,127 +1,159 @@
-// -------------------------------------------------------------  
+// -------------------------------------------------------------
 // --- Gulp Settings ---
 // -------------------------------------------------------------
 
 var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     sass        = require('gulp-sass'),
+    jade        = require('gulp-jade'),
     csso        = require('gulp-csso'),
     uglify      = require('gulp-uglify'),
-    jade        = require('gulp-jade'),
     concat      = require('gulp-concat'),
-    livereload  = require('gulp-livereload'),
-    imagemin    = require('gulp-imagemin'),
     gzip        = require('gulp-gzip'),
-    ftp         = require('gulp-ftp'),
-    pngcrush    = require('imagemin-pngcrush'),
     tinylr      = require('tiny-lr'),
     express     = require('express'),
     app         = express(),
-    marked      = require('marked'),
     include     = require('gulp-include'),
     path        = require('path'),
     neat        = require('node-neat').includePaths,
-    critical    = require('critical'),
     rename      = require('gulp-rename'),
-    psi         = require('psi'),
-    site        = '',
-    key         = '',
+    sourcemaps  = require('gulp-sourcemaps'),
+    plumber     = require('gulp-plumber'),
+    rename      = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    colors      = require('colors'),
+    browserSync = require('browser-sync').create(),
+    imagemin    = require('gulp-imagemin'),
+    pngcrush    = require('imagemin-pngcrush')
     server      = tinylr();
 
-// -------------------------------------------------------------  
+// -------------------------------------------------------------
 // --- Asset Paths src/dist/build ---
-// -------------------------------------------------------------  
-
-var basePaths = {
-    srcPath: 'src/assets/',
-    distPath: 'dist/assets/',
-    buildPath: 'build/assets/'
-};
+// -------------------------------------------------------------
 
 var paths = {
-  images: {
-      src: basePaths.srcPath + 'images/',
-      dist: basePaths.distPath + 'images/',
-      build: basePaths.buildPath + 'images/'
-  },
   styles: {
-    src: basePaths.srcPath + 'stylesheets/',
-    dist: basePaths.distPath + 'css/',
-    build: basePaths.buildPath + 'css/',
-    critical: basePaths.buildPath + 'styles/'
+    src: 'src/assets/stylesheets/',
+    dist: 'dist/assets/css/',
+    build: 'build/assets/css'
   },
   scripts: {
-    src: basePaths.srcPath + 'scripts/',
-    dist: basePaths.distPath + 'js/',
-    build: basePaths.buildPath + 'js/'
+    src: 'src/assets/scripts/',
+    dist: 'dist/assets/js/',
+    build: 'build/assets/js'
   },
   fonts: {
-    src: basePaths.srcPath + 'fonts/',
-    dist: basePaths.distPath + 'fonts/',
-    build: basePaths.buildPath + 'fonts/'
+    src: 'src/assets/fonts/',
+    dist: 'dist/assets/fonts/',
+    build: 'build/assets/fonts'
+  },
+  images: {
+    src: 'src/assets/images/',
+    dist: 'dist/assets/images/',
+    build: 'build/assets/images'
+  },
+  templates: {
+    src: 'src/views/',
+    dist: 'dist/',
+    build: 'build'
   }
 };
 
-// -------------------------------------------------------------  
-// --- JS Files ---
-// -------------------------------------------------------------  
+// -------------------------------------------------------------
+// --- Bower File Lists for SCSS, JS, IE, FONTS, etc. ---
+// -------------------------------------------------------------
 
-var jsFile = paths.scripts.src + 'app.js';
+var bowerPath = 'bower_components/';
 
-// -------------------------------------------------------------  
+var cssFiles = [
+  paths.styles.src,
+  bowerPath+'jeet/scss',
+  bowerPath+'slick.js/slick/'
+  ];
+
+var jsFiles = [
+  bowerPath+'jquery/dist/jquery.min.js',
+  bowerPath+'slick.js/slick/slick.js',
+  paths.scripts.src + 'app.js'
+  ];
+
+var ieFiles = [
+  bowerPath+'html5shiv/dist/html5shiv.js',
+  bowerPath+'selectivizr/selectivizr.js'
+  ];
+
+var fontFiles = [
+  'fonts/*'
+  ];
+
+// -------------------------------------------------------------
 // --- Basic Tasks ---
 // -------------------------------------------------------------
 
 gulp.task('css', function() {
+
+  var onError = function (err) {
+    console.log("[SASS Error]".red);
+    console.log("in: "+err.fileName);
+    console.log("on line: "+err.lineNumber);
+    console.log("message: "+(err.message).red);
+    this.emit('end');
+  };
+
   return gulp.src(paths.styles.src + '*.scss')
-    .pipe( 
-      sass( { 
-        includePaths: [paths.styles.src].concat(neat),
-        errLogToConsole: true
-      } ) )
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: cssFiles.concat(neat)
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
     .pipe( gulp.dest(paths.styles.dist) )
-    .pipe( livereload( server ));
-});
-
-gulp.task('js', function() {
-  return gulp.src(jsFile)
-    .pipe( include() )
-    .pipe( concat('all.js'))
-    .pipe( gulp.dest(paths.scripts.dist))
-    .pipe( livereload( server ));
-});
-
-gulp.task('ie', function() {
-  return gulp.src(paths.scripts.src + '/ie/*.js')
-    .pipe( gulp.dest(paths.scripts.dist + '/ie/'))
-    .pipe( livereload( server ));
-});
-
-gulp.task('images', function() {
-  return gulp.src(paths.images.src + '**/*')
-    .pipe( gulp.dest(paths.images.dist))
-    .pipe( livereload( server ));
+    .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
-  return gulp.src(paths.fonts.src + '**/*')
-    .pipe( gulp.dest(paths.fonts.dist))
-    .pipe( livereload( server ));
+  return gulp.src(fontFiles)
+    .pipe( gulp.dest(paths.fonts.dist));
+});
+
+gulp.task('js', function() {
+  return gulp.src(jsFiles)
+    .pipe( include() )
+    .pipe( concat('all.js'))
+    .pipe( gulp.dest(paths.scripts.dist));
+});
+
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+
+gulp.task('images', function() {
+  return gulp.src(paths.images.src + '**/*')
+    .pipe( gulp.dest(paths.images.dist));
+});
+
+gulp.task('ie', function() {
+  return gulp.src(ieFiles)
+    .pipe( gulp.dest(paths.scripts.dist + '/ie/'));
 });
 
 gulp.task('templates', function() {
-  return gulp.src('src/views/*.jade')
+  return gulp.src(paths.templates.src + '*.jade')
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('dist/'))
-    .pipe( livereload( server ));
+    .pipe(gulp.dest(paths.templates.dist));
 });
 
-gulp.task('express', function() {
-  app.use(express.static(path.resolve('./dist')));
-  app.listen(1337);
-  gutil.log('Listening on port: 1337');
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "dist/"
+    }
+  });
 });
 
 gulp.task('watch', function () {
@@ -131,35 +163,49 @@ gulp.task('watch', function () {
     }
 
     gulp.watch(paths.styles.src + '**/*.scss',['css']);
-    gulp.watch(paths.scripts.src + '**/*.js',['js']);
-    gulp.watch(paths.images.src + '**/*.*',['images']);
-    gulp.watch('src/**/*.jade',['templates']);
-    
+    gulp.watch(paths.scripts.src + '**/*.js',['js-watch']);
+
   });
 });
 
-gulp.task('default', ['js', 'ie', 'images', 'fonts', 'css','templates','express','watch']);
+gulp.task('default', ['js', 'ie','css', 'templates', 'images', 'fonts','watch', 'browser-sync']);
 
-// -------------------------------------------------------------  
+// -------------------------------------------------------------
 // --- Production Build Tasks ---
 // -------------------------------------------------------------
 
 gulp.task('css-prod', function() {
   return gulp.src(paths.styles.src + '*.scss')
-    .pipe( 
-      sass( { 
-        includePaths: [paths.styles.src].concat(neat),
+    .pipe(
+      sass( {
+        includePaths: cssFiles.concat(neat),
       } ) )
+    .pipe( gulp.dest(paths.styles.dist) )
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
     .pipe( csso() )
     .pipe( gulp.dest(paths.styles.build) );
 });
 
 gulp.task('js-prod', function() {
-  return gulp.src(jsFile)
+  return gulp.src(jsFiles)
     .pipe( include() )
     .pipe( uglify() )
-    .pipe( concat('all.js'))
+    .pipe( concat('all.min.js'))
     .pipe( gulp.dest(paths.scripts.build));
+});
+
+gulp.task('templates-prod', function() {
+  return gulp.src(paths.templates.src + '*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(paths.templates.build));
 });
 
 gulp.task('images-prod', function() {
@@ -168,104 +214,25 @@ gulp.task('images-prod', function() {
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
       use: [pngcrush()]
-    }))
+      }))
     .pipe( gulp.dest(paths.images.build));
 });
 
-gulp.task('ie-prod', function() {
-  return gulp.src(paths.scripts.src + '/ie/*.js')
-    .pipe( uglify() )
-    .pipe( gulp.dest(paths.scripts.build + '/ie/'));
-});
-
 gulp.task('fonts-prod', function() {
-  return gulp.src(paths.fonts.src + '**/*')
+  return gulp.src(fontFiles)
     .pipe( gulp.dest(paths.fonts.build));
 });
 
-gulp.task('templates-prod', function() {
-  return gulp.src('src/views/*.jade')
-    .pipe(jade({}))
-    .pipe(gulp.dest('build/'));
-});
-
 gulp.task('compress-js', ['js-prod'], function() {
-  return gulp.src(paths.scripts.build + 'all.js')
+  return gulp.src(paths.scripts.build + 'all.min.js')
     .pipe(gzip())
     .pipe(gulp.dest(paths.scripts.build));
 });
 
 gulp.task('compress-css', ['css-prod'], function() {
-  return gulp.src(paths.styles.build + '*.css')
+  return gulp.src(paths.styles.build + 'all.min.css')
     .pipe(gzip())
     .pipe(gulp.dest(paths.styles.build));
 });
 
-gulp.task('compress-html', ['templates-prod'], function() {
-  return gulp.src('build/*.html')
-    .pipe(gzip())
-    .pipe(gulp.dest('build/'));
-});
-
-gulp.task('build', ['js-prod', 'ie-prod', 'images-prod', 'fonts-prod', 'css-prod','templates-prod', 'compress-js', 'compress-css', 'compress-html']);
-
-// -------------------------------------------------------------
-// Critical Path CSS
-// -------------------------------------------------------------
-
-gulp.task('copystyles', function () {
-  return gulp.src([paths.styles.build +'all.css'])
-    .pipe(rename({
-      basename: "main"
-    }))
-    .pipe(gulp.dest(paths.styles.critical));
-});
-
-
-gulp.task('critical', ['build', 'copystyles'], function () {
-  critical.generateInline({
-    base: 'build/',
-    src: 'index.html',
-    styleTarget: 'assets/styles/main.css',
-    htmlTarget: 'index-critical.html',
-    width: 375,
-    height: 667,
-      minify: true
-  });
-});
-
-// -------------------------------------------------------------  
-// --- Deployment ---
-// -------------------------------------------------------------
-
-gulp.task('deploy', ['build'], function () {
-  return gulp.src('build/**/*')
-    .pipe(ftp({
-      host: 'hostname',
-      user: 'user',
-      pass: '****',
-      remotePath: '/'
-  }));
-});
-
-// -------------------------------------------------------------  
-// --- Page Speed Insights Test ---
-// -------------------------------------------------------------
-
-gulp.task('mobile', function (cb) {
-  psi({
-    // key: key
-    nokey: 'true',
-    url: site,
-    strategy: 'mobile',
-  }, cb);
-});
-
-gulp.task('desktop', function (cb) {
-  psi({
-    // key: key,
-    nokey: 'true',
-    url: site,
-    strategy: 'desktop',
-  }, cb);
-});
+gulp.task('build', ['compress-js', 'compress-css', 'templates-prod', 'images-prod']);
